d94662ccc8ea30003839a254c42df483029c8d1e
Shivam-agarawal <1256shivamagrawal@gmail.com>
2026-02-25 20:36:22 +0530

    sidebar completed

--- back-end/src/middleware/auth.middleware.js
+++ back-end/src/middleware/auth.middleware.js
@@ -5,18 +5,25 @@ import { ENV } from "../lib/env.js";
 export const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
-    if (!token) return res.status(401).json({ message: "Unauthorized - No token provided" });
+    if (!token)
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - No token provided" });
 
     const decoded = jwt.verify(token, ENV.JWT_SECRET);
-    if (!decoded) return res.status(401).json({ message: "Unauthorized - Invalid token" });
+    if (!decoded)
+      return res.status(401).json({ message: "Unauthorized - Invalid token" });
 
-    const user = await User.findById(decoded.userId).select("-password");
-    if (!user) return res.status(404).json({ message: "User not found" });
+    const user = await User.findById(decoded.id).select("-password");
+    if (!user) {
+      console.log("User not found for userId:", decoded.id);
+      return res.status(404).json({ message: "User not found" });
+    }
 
     req.user = user;
     next();
   } catch (error) {
-    console.log("Error in protectRoute middleware:", error);
+    console.log("Error in protectRoute middleware:", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
 };
